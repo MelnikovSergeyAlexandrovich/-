7678a57d3eda871cdad230076f3cab4268d4ff84
MelnikovSergeyAlexandrovich <isip_s.a.melnikov@gmail.com>
2023-03-18 14:55:48 +0300

    pobeda)

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using FirstPracticeTask.dataWPFDataSetTableAdapters;
+
+namespace FirstPracticeTask
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        pizzasTableAdapter Pizza = new pizzasTableAdapter();
+        piesTableAdapter Pie = new piesTableAdapter();
+        bakeryShopTableAdapter BakeryShop = new bakeryShopTableAdapter();   
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.BakeryData.ItemsSource = BakeryShop.GetData();
+            window2.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.PizzasData.ItemsSource = Pizza.GetData();
+            window1.Show();
+        }
+
+        private void PiesDataBaseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window3 window3 = new Window3();
+            window3.PiesData.ItemsSource = Pie.GetData();
+            window3.Show();
+        }
+    }
+}
